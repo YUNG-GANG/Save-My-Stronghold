37c2ee69baa243bc1d355e64c6303a2013abd955
Nicholas Young <ndy14@ufl.edu>
2019-12-18 11:18:33 -0500

    Boilerplate mod setup

---
+++ src/main/java/com/yungnickyoung/minecraft/savemystronghold/SaveMyStronghold.java
@@ -0,0 +1,8 @@
+package com.yungnickyoung.minecraft.savemystronghold;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "savemystronghold", useMetadata = true, acceptableRemoteVersions = "*")
+public class SaveMyStronghold {
+
+}
