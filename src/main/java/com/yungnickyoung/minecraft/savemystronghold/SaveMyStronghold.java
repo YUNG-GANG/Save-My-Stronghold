8744dbada4ebefae84cabcded6c723803d99a763
Nicholas Young <ndy14@ufl.edu>
2021-02-01 01:38:12 -0500

    Fabric port! v1.0.

--- src/main/java/com/yungnickyoung/minecraft/savemystronghold/SaveMyStronghold.java
+++ src/main/java/com/yungnickyoung/minecraft/savemystronghold/SaveMyStronghold.java
@@ -1,16 +1,18 @@
 package com.yungnickyoung.minecraft.savemystronghold;
 
-import net.minecraftforge.fml.common.Mod;
+import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
  * Save My Stronghold! mod entry point
  */
-@Mod("savemystronghold")
-public class SaveMyStronghold {
+public class SaveMyStronghold implements ModInitializer {
     public static final String MOD_ID = "savemystronghold";
     public static final Logger LOGGER = LogManager.getLogger("savemystronghold");
 
     public SaveMyStronghold() {}
+
+    @Override
+    public void onInitialize() {}
 }
--- src/main/java/com/yungnickyoung/minecraft/savemystronghold/mixin/StrongholdMixin.java
+++ src/main/java/com/yungnickyoung/minecraft/savemystronghold/mixin/StrongholdMixin.java
@@ -1,28 +1,29 @@
 package com.yungnickyoung.minecraft.savemystronghold.mixin;
 
+import com.yungnickyoung.minecraft.savemystronghold.SaveMyStronghold;
 import net.minecraft.block.BlockState;
-import net.minecraft.util.math.MutableBoundingBox;
-import net.minecraft.world.ISeedReader;
-import net.minecraft.world.gen.feature.structure.IStructurePieceType;
-import net.minecraft.world.gen.feature.structure.StrongholdPieces;
-import net.minecraft.world.gen.feature.structure.StructurePiece;
+import net.minecraft.structure.StrongholdGenerator;
+import net.minecraft.structure.StructurePiece;
+import net.minecraft.structure.StructurePieceType;
+import net.minecraft.util.math.BlockBox;
+import net.minecraft.world.StructureWorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 
 import java.util.Random;
 
-@Mixin(StrongholdPieces.Stronghold.class)
+@Mixin(StrongholdGenerator.Piece.class)
 public abstract class StrongholdMixin extends StructurePiece {
-    protected StrongholdMixin(IStructurePieceType p_i51342_1_, int p_i51342_2_) {
-        super(p_i51342_1_, p_i51342_2_);
+    protected StrongholdMixin(StructurePieceType type, int length) {
+        super(type, length);
     }
 
     @Override
-    protected void fillWithBlocks(ISeedReader worldIn, MutableBoundingBox boundingboxIn, int xMin, int yMin, int zMin, int xMax, int yMax, int zMax, BlockState boundaryBlockState, BlockState insideBlockState, boolean existingOnly) {
-        super.fillWithBlocks(worldIn, boundingboxIn, xMin, yMin, zMin, xMax, yMax, zMax, boundaryBlockState, insideBlockState, false);
+    protected void fillWithOutline(StructureWorldAccess structureWorldAccess, BlockBox blockBox, int minX, int minY, int minZ, int maxX, int maxY, int maxZ, BlockState outline, BlockState inside, boolean cantReplaceAir) {
+        super.fillWithOutline(structureWorldAccess, blockBox, minX, minY, minZ, maxX, maxY, maxZ, outline, inside, false);
     }
 
     @Override
-    protected void fillWithRandomizedBlocks(ISeedReader worldIn, MutableBoundingBox boundingboxIn, int minX, int minY, int minZ, int maxX, int maxY, int maxZ, boolean alwaysReplace, Random rand, StructurePiece.BlockSelector blockselector) {
-        super.fillWithRandomizedBlocks(worldIn, boundingboxIn, minX, minY, minZ, maxX, maxY, maxZ, false, rand, blockselector);
+    protected void fillWithOutline(StructureWorldAccess structureWorldAccess, BlockBox blockBox, int minX, int minY, int minZ, int maxX, int maxY, int maxZ, boolean cantReplaceAir, Random random, BlockRandomizer blockRandomizer) {
+        super.fillWithOutline(structureWorldAccess, blockBox, minX, minY, minZ, maxX, maxY, maxZ, false, random, blockRandomizer);
     }
 }
